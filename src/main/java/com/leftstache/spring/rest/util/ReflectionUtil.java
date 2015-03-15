79b86e979037ee00c967d1974258bf62a57a790d
Joel Johnson <johnson.joel.86@gmail.com>
2015-03-14 23:25:55 -0600

    updating reflection util to work
    unfortunately this uses magic from the sun package :( but I can't see a better way to do it.

--- src/main/java/com/leftstache/spring/rest/util/ReflectionUtil.java
+++ src/main/java/com/leftstache/spring/rest/util/ReflectionUtil.java
@@ -1,5 +1,7 @@
 package com.leftstache.spring.rest.util;
 
+import sun.reflect.generics.reflectiveObjects.*;
+
 import java.lang.reflect.*;
 
 /**
@@ -12,11 +14,12 @@ public final class ReflectionUtil {
 		while(currentClass != null && currentClass != Object.class) {
 			Type[] genericInterfaces = currentClass.getGenericInterfaces();
 			for (Type genericInterface : genericInterfaces) {
-				if (genericInterface.getTypeName().startsWith(targetInterface.getName() + "<")) {
-					if (genericInterface instanceof ParameterizedType) {
-						return (Class) ((ParameterizedType) genericInterface).getActualTypeArguments()[index];
+				if(genericInterface instanceof ParameterizedTypeImpl) {
+					ParameterizedTypeImpl parameterizedType = (ParameterizedTypeImpl) genericInterface;
+					Class<?> rawType = parameterizedType.getRawType();
+					if(targetInterface.isAssignableFrom(rawType)) {
+						return (Class) parameterizedType.getActualTypeArguments()[index];
 					}
-					break;
 				} else if(genericInterface instanceof Class) {
 					Class entityType = findGenericType((Class) genericInterface, targetInterface, index);
 					if(entityType != null) {
