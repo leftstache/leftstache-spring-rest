5bc97c80e531ab1f8810e2abe1e4f982148e2d5b
Joel Johnson <johnson.joel.86@gmail.com>
2015-03-19 00:13:08 -0600

    simple and/or queries

--- src/main/java/com/leftstache/spring/rest/util/SearchUtil.java
+++ src/main/java/com/leftstache/spring/rest/util/SearchUtil.java
@@ -1,7 +1,6 @@
 package com.leftstache.spring.rest.util;
 
 import org.springframework.data.jpa.domain.*;
-import org.springframework.stereotype.*;
 
 import javax.persistence.criteria.*;
 import java.util.*;
@@ -10,9 +9,58 @@ import java.util.*;
  * @author Joel Johnson
  */
 public final class SearchUtil {
+
+	public static final String OR_KEY = "$or";
+	public static final String GREATER_THAN_KEY = "$gt";
+	public static final String LESS_THAN_KEY = "$lt";
+	public static final String GREATER_THAN_EQUAL_KEY = "$gte";
+	public static final String LESS_THAN_EQUAL_KEY = "$lte";
+
 	public static Specification generateSearchQuery(final Map<String, Object> queryMap) {
 		Specification specification = new DynamicSearchSpecification(queryMap);
 		return specification;
+	}
+
+	private static Predicate toPredicate(Root root, CriteriaQuery query, CriteriaBuilder cb, Map<String, Object> queryMap) {
+		Predicate lastPredicate = null;
+		for (Map.Entry<String, Object> entry : queryMap.entrySet()) {
+			String key = entry.getKey();
+			Object value = entry.getValue();
+
+			if(OR_KEY.equals(key)) {
+				if(value instanceof List) {
+					Predicate orPredicate = null;
+					for (Object v : (List)value) {
+						if(!(v instanceof Map)) {
+							throw new UnsupportedOperationException("Expected object in " + OR_KEY + " list, but was " + v);
+						}
+						Predicate currentPredicate = toPredicate(root, query, cb, (Map<String, Object>) v);
+						if(orPredicate != null) {
+							orPredicate = cb.or(orPredicate, currentPredicate);
+						} else {
+							orPredicate = currentPredicate;
+						}
+					}
+					if(lastPredicate != null) {
+						lastPredicate = cb.and(lastPredicate, orPredicate);
+					} else {
+						lastPredicate = orPredicate;
+					}
+				} else {
+					throw new QueryException(OR_KEY + " should reference a list");
+				}
+			} else if(value instanceof List) {
+				throw new QueryException("List comparisons are not currently supported");
+			} else {
+				Predicate currentPredicate = cb.equal(root.get(key), value);
+				if (lastPredicate != null) {
+					lastPredicate = cb.and(lastPredicate, currentPredicate);
+				} else {
+					lastPredicate = currentPredicate;
+				}
+			}
+		}
+		return lastPredicate;
 	}
 
 	private static class DynamicSearchSpecification implements Specification {
@@ -24,26 +72,13 @@ public final class SearchUtil {
 
 		@Override
 		public Predicate toPredicate(Root root, CriteriaQuery query, CriteriaBuilder cb) {
-			Predicate lastPredicate = null;
-			for (Map.Entry<String, Object> entry : queryMap.entrySet()) {
-				String key = entry.getKey();
-				Object value = entry.getValue();
+			return SearchUtil.toPredicate(root, query, cb, queryMap);
+		}
+	}
 
-				Predicate currentPredicate;
-				Path variable = root.get(key);
-				if(value == null) {
-					currentPredicate = cb.isNotNull(variable);
-				} else {
-					currentPredicate = cb.equal(variable, value);
-				}
-
-				if(lastPredicate == null) {
-					lastPredicate = currentPredicate;
-				} else if(currentPredicate != null) {
-					lastPredicate = cb.and(lastPredicate, currentPredicate);
-				}
-			}
-			return lastPredicate;
+	public static class QueryException extends RuntimeException {
+		public QueryException(String message) {
+			super(message);
 		}
 	}
 
