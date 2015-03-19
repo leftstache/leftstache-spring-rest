af26527961dce478e4506825735ca447f31963d4
Joel Johnson <johnson.joel.86@gmail.com>
2015-03-19 00:26:53 -0600

    implementing various comparisons

--- src/main/java/com/leftstache/spring/rest/util/SearchUtil.java
+++ src/main/java/com/leftstache/spring/rest/util/SearchUtil.java
@@ -11,6 +11,9 @@ import java.util.*;
 public final class SearchUtil {
 
 	public static final String OR_KEY = "$or";
+
+	public static final String EQUAL_KEY = "$eq";
+	public static final String NOT_EQUAL_KEY = "$ne";
 	public static final String GREATER_THAN_KEY = "$gt";
 	public static final String LESS_THAN_KEY = "$lt";
 	public static final String GREATER_THAN_EQUAL_KEY = "$gte";
@@ -52,7 +55,44 @@ public final class SearchUtil {
 			} else if(value instanceof List) {
 				throw new QueryException("List comparisons are not currently supported");
 			} else {
-				Predicate currentPredicate = cb.equal(root.get(key), value);
+				Predicate currentPredicate = null;
+				if(value instanceof Map) {
+					Map<String, Object> nestedMap = (Map) value;
+					for (Map.Entry<String, Object> nestedEntry : nestedMap.entrySet()) {
+						String nestedKey = nestedEntry.getKey();
+						Object nestedValue = nestedEntry.getValue();
+
+						Predicate comparePredicate;
+						if(EQUAL_KEY.equals(nestedKey)) {
+							comparePredicate = cb.equal(root.get(key), nestedValue);
+						} else if(NOT_EQUAL_KEY.equals(nestedKey)) {
+							comparePredicate = cb.notEqual(root.get(key), nestedValue);
+						} else if(LESS_THAN_KEY.equals(nestedKey)) {
+							Expression<Comparable> path = root.get(key);
+							comparePredicate = cb.lessThan(path, (Comparable) nestedValue);
+						} else if(GREATER_THAN_KEY.equals(nestedKey)) {
+							Expression<Comparable> path = root.get(key);
+							comparePredicate = cb.greaterThan(path, (Comparable) nestedValue);
+						} else if(LESS_THAN_EQUAL_KEY.equals(nestedKey)) {
+							Expression<Comparable> path = root.get(key);
+							comparePredicate = cb.lessThanOrEqualTo(path, (Comparable) nestedValue);
+						} else if(GREATER_THAN_EQUAL_KEY.equals(nestedKey)){
+							Expression<Comparable> path = root.get(key);
+							comparePredicate = cb.greaterThanOrEqualTo(path, (Comparable)nestedValue);
+						} else {
+							throw new QueryException("Unsupported: " + nestedKey);
+						}
+
+						if(currentPredicate == null) {
+							currentPredicate = comparePredicate;
+						} else {
+							currentPredicate = cb.and(currentPredicate, comparePredicate);
+						}
+					}
+				} else {
+					currentPredicate = cb.equal(root.get(key), value);
+				}
+
 				if (lastPredicate != null) {
 					lastPredicate = cb.and(lastPredicate, currentPredicate);
 				} else {
